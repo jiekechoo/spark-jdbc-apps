84f7f39db826cc417263f42f7d98a8219e50c8f7
jiekechoo <jiekechoo@126.com>
2016-01-06 07:59:09 +0800

    保存到jdbc模块修改

--- spark-save-to-db/src/main/java/com/sectong/Main.java
+++ spark-save-to-db/src/main/java/com/sectong/Main.java
@@ -1,35 +1,38 @@
 package com.sectong;
 
 import java.io.Serializable;
+import java.util.Properties;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.SaveMode;
 
 public class Main implements Serializable {
 
-    private static final String MYSQL_USERNAME = "demo";
-    private static final String MYSQL_PWD = "demo";
-    private static final String MYSQL_CONNECTION_URL =
-            "jdbc:mysql://192.168.1.91:3306/demo?user=" + MYSQL_USERNAME + "&password=" + MYSQL_PWD;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8513279306224995844L;
+	private static final String MYSQL_USERNAME = "demo";
+	private static final String MYSQL_PWD = "demo";
+	private static final String MYSQL_CONNECTION_URL = "jdbc:mysql://192.168.1.91:3306/demo";
 
-    private static final JavaSparkContext sc =
-            new JavaSparkContext(new SparkConf().setAppName("SparkSaveToDb").setMaster("local[*]"));
+	private static final JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("SparkSaveToDb").setMaster("local[*]"));
 
-    private static final SQLContext sqlContext = new SQLContext(sc);
+	private static final SQLContext sqlContext = new SQLContext(sc);
 
-    public static void main(String[] args) {
-        //Sample data-frame loaded from a JSON file
-        DataFrame usersDf = sqlContext.jsonFile("users.json");
+	public static void main(String[] args) {
+		// Sample data-frame loaded from a JSON file
+		DataFrame usersDf = sqlContext.read().json("users.json");
 
-        //Save data-frame to MySQL (or any other JDBC supported databases)
-        //Choose one of 2 options depending on your requirement (Not both).
+		// Save data-frame to MySQL (or any other JDBC supported databases)
+		Properties connectionProperties = new Properties();
+		connectionProperties.put("user", MYSQL_USERNAME);
+		connectionProperties.put("password", MYSQL_PWD);
 
-        //Option 1: Create new table and insert all records.
-        usersDf.createJDBCTable(MYSQL_CONNECTION_URL, "users", true);
-
-        //Option 2: Insert all records to an existing table.
-        usersDf.insertIntoJDBC(MYSQL_CONNECTION_URL, "users", false);
-    }
+		// write dataframe to jdbc mysql
+		usersDf.write().mode(SaveMode.Append).jdbc(MYSQL_CONNECTION_URL, "users", connectionProperties);
+	}
 }
